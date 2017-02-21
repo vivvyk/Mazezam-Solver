f9da79983055282866c21cf60288d8b9fa6f6383
VivekKumar <vivekkumar@localhost.localdomain>
2017-02-20 23:35:41 -0500

    Adding Solve, Mechanics, and Level

---
+++ Mechanics.py
@@ -0,0 +1,202 @@
+__author__ = "Vivek Kumar"
+import numpy as np
+
+'''Columns and Rows'''
+column_number = None
+row_number = None
+
+'''Goal Position'''
+gpos = (None,None) #As indexes, (1,6)
+
+def read(level):
+    '''
+    Takes text encoding and returns matrix representation of state.
+    We assume that the matrix is rectangular.
+    '''
+    levString = level.read()
+    split = levString.split('\n')
+    for (index,item) in enumerate(split):
+        if item == '#' * len(item):
+            split.pop(index)
+    split.pop(-1)
+
+    cutIndex1 = 0
+    cutIndex2 = 0
+    levelMatrix = []
+    for row in split:
+        matrow = []
+        for (index,character) in enumerate(row):
+            matrow.append(character)
+            if character == "+":
+                cutIndex1 = index
+                continue
+            if character == "*":
+                cutIndex2 = index
+                continue
+            else:
+                pass
+        levelMatrix.append(matrow)
+
+    for row in levelMatrix:
+        for (index, character) in enumerate(row):
+            if character == "+":
+                row[index+1] = 2
+            if character == ".":
+                row[index] = 0
+            if character == "L" or character == "R" or character == "C":
+                row[index] = 1
+
+    state = []
+    for i in range(len(levelMatrix)):
+        state.append(levelMatrix[i][cutIndex1+1:cutIndex2])
+
+    return np.array(state)
+
+def down(matrix_representation, LCFS=False):
+    '''
+    Moves the character down, if possible.
+    '''
+    column_counter = 0
+    while column_counter < column_number:
+        row_counter = 0
+        while row_counter < row_number:
+            if matrix_representation[row_counter][column_counter] == 2:
+                if row_counter + 1 < row_number and matrix_representation[row_counter+1][column_counter] != 1:
+                    matrix_representation[row_counter+1][column_counter] = 2
+                    matrix_representation[row_counter][column_counter] = 0
+                    if LCFS == False:
+                        return matrix_representation
+                    else:
+                        return (0,matrix_representation)
+            row_counter += 1
+        column_counter += 1
+    return None
+
+def up(matrix_representation, LCFS=False):
+    '''
+    Moves the character up, if possible.
+    '''
+    column_counter = 0
+    while column_counter < column_number:
+        row_counter = 0
+        while row_counter < row_number:
+            if matrix_representation[row_counter][column_counter] == 2:
+                if (row_counter - 1 < 0) == False and matrix_representation[row_counter - 1][column_counter] != 1:
+                    matrix_representation[row_counter - 1][column_counter] = 2
+                    matrix_representation[row_counter][column_counter] = 0
+                    if LCFS == False:
+                        return matrix_representation
+                    else:
+                        return (0,matrix_representation)
+            row_counter += 1
+        column_counter += 1
+    return None
+
+def right(matrix_representation,LCFS=False):
+    '''
+    Moves the character right, if possible
+    '''
+    for row in range(row_number):
+        for column in range(column_number):
+            if matrix_representation[row][column] == 2:
+                if (column + 1) == column_number or (matrix_representation[row][-1] == 1 and matrix_representation[row][column+1] == 1):
+                    return None
+                elif matrix_representation[row][column+1] == 0:
+                    matrix_representation[row][column] = 0
+                    matrix_representation[row][column+1] = 2
+                    if LCFS == False:
+                        return matrix_representation
+                    else:
+                        return (0,matrix_representation)
+                elif matrix_representation[row][column+1] == 1:
+                    newrow = np.roll(matrix_representation[row],1) #Rotates array
+                    matrix_representation[row] = newrow
+                    if LCFS == False:
+                        return matrix_representation
+                    else:
+                        return (1,matrix_representation)
+    return None
+
+def left(matrix_representation,LCFS=False):
+    '''
+    Moves the character left, if possible.
+    '''
+    for row in range(row_number):
+        for column in range(column_number):
+            if matrix_representation[row][column] == 2:
+                if (column - 1) < 0 or (matrix_representation[row][0] == 1 and matrix_representation[row][column-1] == 1):
+                    return None
+                elif matrix_representation[row][column-1] == 0:
+                    matrix_representation[row][column] = 0
+                    matrix_representation[row][column-1] = 2
+                    if LCFS == False:
+                        return matrix_representation
+                    else:
+                        return (0,matrix_representation)
+                elif matrix_representation[row][column-1] == 1:
+                    newrow = np.roll(matrix_representation[row],-1) #Rotates array
+                    matrix_representation[row] = newrow
+                    if LCFS == False:
+                        return matrix_representation
+                    else:
+                        return (1,matrix_representation)
+    return None
+
+def neighbors(vertex, LCFS=False):
+    '''
+    Makes 4 copies of vertex, and runs each function on them. Returns if successful.
+    If LCFS is True, then a neighbor_list will be a list of tuples.
+    '''
+    if LCFS == False:
+        neighbor_list = []
+        v1 = np.copy(vertex)
+        v2 = np.copy(vertex)
+        v3 = np.copy(vertex)
+        v4 = np.copy(vertex)
+        if up(v1) is not None:
+            neighbor_list.append(v1)
+        if down(v2) is not None:
+            neighbor_list.append(v2)
+        if right(v3) is not None:
+            neighbor_list.append(v3)
+        if left(v4) is not None:
+            neighbor_list.append(v4)
+        return neighbor_list
+
+    elif LCFS == True:
+        neighbor_list1 = []
+        v5 = np.copy(vertex)
+        v6 = np.copy(vertex)
+        v7 = np.copy(vertex)
+        v8 = np.copy(vertex)
+        vfake1 = np.copy(v5)
+        vfake2 = np.copy(v6)
+        vfake3 = np.copy(v7)
+        vfake4 = np.copy(v8)
+        if up(vfake1) is not None:
+            neighbor_list1.append(up(v5,True))
+        if down(vfake2) is not None:
+            neighbor_list1.append(down(v6,True))
+        if right(vfake3) is not None:
+            neighbor_list1.append(right(v7,True))
+        if left(vfake4) is not None:
+            neighbor_list1.append(left(v8,True))
+        return neighbor_list1
+
+
+def is_goal(A):
+    '''
+    Tests for goal.
+    '''
+    return A[gpos[0]-1][gpos[1]-1] == 2
+
+def cost(path):
+    '''
+    Finds the total cost of a path.
+    '''
+    cost = 0
+    newpath = []
+    for item in path:
+        cost += item[0]
+        newpath.append(item[-1])
+    return (cost, newpath)
---
+++ Solve.py
@@ -0,0 +1,215 @@
+__author__ = "Vivek Kumar"
+import Mechanics as m
+import numpy as np
+
+m.column_number = 7
+m.row_number = 3
+
+m.gpos = (2,7)
+
+def BFS(start):
+    '''
+    Breadth-First Search
+    Utilizes the frontier as a priority queue.
+    '''
+    frontier = []
+    frontier.append([start])
+    while len(frontier) > 0:
+        path = frontier.pop(0)
+        last_vertex = path[-1]
+        if m.is_goal(last_vertex):
+            return path
+        for next_vertex in m.neighbors(last_vertex):
+            new_path = path + [next_vertex]
+            frontier.append(new_path)
+    return None
+
+def DFS(start):
+    '''
+    Depth-First Search
+    Utilizes the frontier as a stack.
+    Unfortunately this will often fall into cycles.
+    '''
+    frontier = []
+    frontier.append([start])
+    while len(frontier) > 0:
+        path = frontier.pop() #Removing the zero turns our queue into a stack.
+        last_vertex = path[-1]
+        if is_goal(last_vertex):
+            return path
+        for next_vertex in neighbors(last_vertex):
+            new_path = path + [next_vertex]
+            frontier.append(new_path)
+    return None
+
+def DFS_Enhanced(start):
+    '''
+    Enhanced Depth-First-Search with cycle detection and path pruning.
+    Checks if the vertex is already in the path, or already on another path.s
+    '''
+    frontier = []
+    frontier.append([start])
+    while len(frontier) > 0:
+        path = frontier.pop()
+        last_vertex = path[-1]
+        if m.is_goal(last_vertex):
+            return path
+        for next_vertex in m.neighbors(last_vertex):
+            multpath = False
+            for member in frontier: #Checks if the new vertex is on any previous path. If so, discard path.
+                if any((next_vertex == x).all() for x in member):
+                    multpath = True
+                    break
+            if multpath == True:
+                continue
+            if any((next_vertex == x).all() for x in path): #If new vertex already in path, ignore this neighbor.
+                continue
+            else:
+                new_path = path + [next_vertex]
+                frontier.append(new_path)
+    return None
+
+def DFS_bound(start,depth):
+    '''
+     Depth = Max number of moves to solve problem, using cycle detection and path pruning
+     If the length of the path is past a maximum depth, the function will cease to increase this path.
+     '''
+    frontier = []
+    frontier.append([start])
+    while len(frontier) > 0:
+        path = frontier.pop()
+        last_vertex = path[-1]
+        if m.is_goal(last_vertex):
+            return path
+        if (len(path)) == depth: #Discard path if more than depth steps and the end vertex is not a goal
+            continue
+        for next_vertex in m.neighbors(last_vertex):
+            multpath = False
+            for member in frontier:
+                if any((next_vertex == x).all() for x in member):
+                    multpath = True
+                    break
+            if multpath == True:
+                continue
+            if any((next_vertex == x).all() for x in path):
+                continue
+            else:
+                new_path = path + [next_vertex]
+                frontier.append(new_path)
+    return None
+
+def Deep(start):
+    '''
+    Iterative Deepening, "continuously bounds" the size of the path.
+    This is a depth-first search algorithm with a bound on each attempt.
+    '''
+    max_length = 0
+    frontier = []
+    while True:
+        if len(frontier) == 0: #Increases allowed path length
+            max_length += 1
+            frontier.append([start])
+        path = frontier.pop()
+        last_vertex = path[-1]
+        if m.is_goal(last_vertex):
+            return path
+        if len(path) == max_length: #Prevents moving further than allowed path length.
+            continue
+        for next_vertex in m.neighbors(last_vertex):
+            multpath = False
+            for member in frontier:
+                if any((next_vertex == x).all() for x in member):
+                    multpath = True
+                    break
+            if multpath == True:
+                continue
+
+            if any((next_vertex == x).all() for x in path):
+                continue
+            else:
+                new_path = path + [next_vertex]
+                frontier.append(new_path)
+    return None
+
+def LCFS(start):
+    '''
+    Finds the path of least cost. We do this by iteratively increasing the cost each iteration of the loop.
+    '''
+    allowed_cost = 0
+    frontier = []
+    while True:
+        if len(frontier) == 0: #Increases allowed cost
+            allowed_cost += 1
+            frontier.append([(0,start)])
+        path = frontier.pop(0) #We implement the frontier as a queue.
+        last_vertex = path[-1]
+        if m.is_goal(last_vertex[-1]):
+            return (m.cost(path), path)
+        if m.cost(path)[0] == allowed_cost: #Prevents moving further than allowed cost.
+            continue
+        for next_vertex in m.neighbors(last_vertex[-1], True):
+            multpath = False
+            for member in frontier:
+                if any((next_vertex[-1] == x[-1]).all() for x in member):
+                    multpath = True
+                    break
+            if multpath == True:
+                continue
+            if any((next_vertex[-1] == x[-1]).all() for x in path):
+                continue
+            else:
+                new_path = path + [next_vertex]
+                frontier.append(new_path)
+    return None
+
+
+if __name__ == "__main__":
+    #Simply comment out any functions you don't want to write output.
+
+    #Prints the level
+    print "\nVERTEX"
+    level = open("Level.txt",'r') #PATH TO FILE
+    lev3 = m.read(level)
+    print lev3
+
+    #BFS often takes time to get to the solution, so we can manually move the person closer to test BFS.
+    print "\nBFS"
+    test = np.array([[1,1,2,0,1,1,0],[0,1,0,1,0,1,0],[1,1,0,1,0,0,0]])
+    path = BFS(test)
+    for vertex in path:
+        print "\n"
+        print vertex
+
+    #Finds the lowest cost solution
+    print "\nLCFS"
+    path = LCFS(lev3)
+    print "Cost: " + str(path[0][0])
+    for vertex in path[0][-1]:
+        print "\n"
+        print vertex
+
+    #Tests DFS_Enhanced because DFS often gets into cycles.
+    print "\nDFS_ENHANCED"
+    path = DFS_Enhanced(lev3)
+    for vertex in path:
+        print "\n"
+        print vertex
+
+    #Places bound on DFS.
+    print "\nDFS_BOUND"
+    path = DFS_bound(lev3,40) #Currently return solution of size 38. Making bound smaller returns solution of size 36, unless bound is made less than 36.
+    if path is not None:
+        for (i, vertex) in enumerate(path):
+            print "\n"
+            print i+1
+            print vertex
+    else:
+        print "No solution in given bound"
+
+    #Iterative Deepening (Will always find the minimum solution, should match DFS_Bound with minimum bound of 36). Also, this is somewhat slow.
+    print "\nITERATIVE DEEPENING"
+    path = Deep(lev3)
+    for (i, vertex) in enumerate(path):
+        print "\n"
+        print i + 1
+        print vertex
